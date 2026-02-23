7db45e8448cbf948785f06af6631b8df5be9d110
devezera.rdz <davi40rodriguez@gmail.com>
2026-02-23 19:38:26 -0300

    medidas

--- Aula06/medidas.cs
+++ Aula06/medidas.cs
@@ -1,19 +1,26 @@
-using System.Security.Cryptography;
+
+using System.Security.Principal;
 using static System.Console;
-//declaração de variaveis
-double a,b,c, areaQuadrada, areaTriangulo, areaTrapezia;
-//Entrada de dados
-Write("Digite a medida A:");
-a = double.Parse(ReadLine());
-Write("DIgite B:");
-b = double.Parse(ReadLine());
-Write("Digite C: ");
-c = double.Parse(ReadLine());
-//Processamento de dados
-areaQuadrada = a * a;
-areaTriangulo =(a * b) / 2;
-areaTrapezio = ((a + b) * c) / 2;
-//Saída de dados
-WriteLine($"A area do quadrado = {areaQuadrada}");
-WriteLine($"A area do triangulo = {areaTriangulo}");
-WriteLine($"A area do trapezio = {areaTrapezio}");
+try{
+    //declaração de variaveis
+    double a,b,c, areaQuadrada, areaTriangulo, areaTrapezio;
+    //Entrada de dados
+    Write("Digite a medida A:");
+    a = double.Parse(ReadLine());
+    Write("DIgite B:");
+    b = double.Parse(ReadLine());
+    Write("Digite C: ");
+    c = double.Parse(ReadLine());
+    //Processamento de dados
+    areaQuadrada = a * a;
+    areaTriangulo =(a * b) / 2;
+    areaTrapezio = ((a +b) *c) / 2;
+    //Saída de dados
+    WriteLine($"A area do quadrado = {areaQuadrada:F4}");
+    WriteLine($"A area do triangulo = {areaTriangulo:F4}");
+    WriteLine($"A area do trapezio = {areaTrapezio:F4}");
+    }
+catch (Exception)
+{
+    WriteLine("Valores invalidos, programa encerrado");
+}
